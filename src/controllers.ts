38bf7a86933070f0f408df37d90112b7a49b23b3
h3nrey <pedro.novaes.education@gmail.com>
2025-01-30 08:35:31 -0300

    feat: creating basic crud(get, list, update and delete)

--- src/controllers.ts
+++ src/controllers.ts
@@ -4,6 +4,34 @@ import { validStatus } from "./utils";
 
 const prisma = new PrismaClient();
 
+async function getTask(req: Request, res: Response): Promise<void>{
+    const { id } = req.params;
+
+  try {
+    const task = await prisma.task.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!task) {
+      res.status(404).json({ error: 'task could not be found with this specific id: ' + id });
+      return;
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ error: 'Error to get the task' });
+  }
+}
+
+async function listTasks(req: Request, res: Response): Promise<void> {
+    try {
+        const tasks = await prisma.task.findMany();
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({ error: 'Error, could not be listed' });
+    }
+}
+
 async function createTask(req: Request, res: Response): Promise<void> {
     const { title, description, status, expireDate }: Task = req.body;
 
@@ -19,7 +47,7 @@ async function createTask(req: Request, res: Response): Promise<void> {
     }
 
     try {
-        const tarefa = await prisma.task.create({
+        const task = await prisma.task.create({
         data: {
             title,
             description,
@@ -28,12 +56,66 @@ async function createTask(req: Request, res: Response): Promise<void> {
         },
         });
 
-        res.status(201).json(tarefa);
+        res.status(201).json(task);
     } catch (error) {
         res.status(500).json({ error: 'Error, task could not be created' });
     }
 }
 
+async function updateTask(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    const { title, description, status, expireDate }: Task = req.body;
+  
+    if (status) {
+      if (!validStatus.includes(status)) {
+        res.status(400).json({ error: 'task with invalid status' });
+        return;
+      }
+    }
+  
+    try {
+      const task = await prisma.task.update({
+        where: { id: Number(id) },
+        data: {
+          title,
+          description,
+          status,
+          expireDate: expireDate ? new Date(expireDate) : undefined,
+        },
+      });
+  
+      res.status(200).json(task);
+    } catch (error) {
+      res.status(500).json({ error: 'Error: task could not be updated' });
+    }
+  };
+
+  async function deleteTask(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+
+    try {
+        const task = await prisma.task.findUnique({
+            where: { id: Number(id) },
+          });
+      
+          if (!task) {
+            res.status(404).json({ error: 'task could not be found with this specific id: ' + id });
+            return;
+          }
+      await prisma.task.delete({
+        where: { id: Number(id) },
+      });
+  
+      res.status(204).send();
+    } catch (error) {
+      res.status(500).json({ error: 'Error: task could not be deleted' });
+    }
+  }
+
 export {
-    createTask
+    createTask,
+    listTasks,
+    getTask,
+    updateTask,
+    deleteTask
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
-import { createTask } from './controllers';
+import { createTask, deleteTask, getTask, listTasks, updateTask } from './controllers';
 
 const router = Router();
 
+router.get("/tarefas/:id", getTask)
+router.get("/tarefas", listTasks)
 router.post("/tarefas", createTask)
+router.put("/tarefas/:id", updateTask)
+router.delete("/tarefas/:id", deleteTask)
+
 export default router;
