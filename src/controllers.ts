a2591a6bcbd15e118ed99f9ce16e987b30d4ed13
h3nrey <pedro.novaes.education@gmail.com>
2025-01-30 08:40:41 -0300

    style: setup prettier

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,11 +1,11 @@
-import { PrismaClient, Task } from "@prisma/client";
-import { Request, Response } from "express";
-import { validStatus } from "./utils";
+import { PrismaClient, Task } from '@prisma/client';
+import { Request, Response } from 'express';
+import { validStatus } from './utils';
 
 const prisma = new PrismaClient();
 
-async function getTask(req: Request, res: Response): Promise<void>{
-    const { id } = req.params;
+async function getTask(req: Request, res: Response): Promise<void> {
+  const { id } = req.params;
 
   try {
     const task = await prisma.task.findUnique({
@@ -13,7 +13,11 @@ async function getTask(req: Request, res: Response): Promise<void>{
     });
 
     if (!task) {
-      res.status(404).json({ error: 'task could not be found with this specific id: ' + id });
+      res
+        .status(404)
+        .json({
+          error: 'task could not be found with this specific id: ' + id,
+        });
       return;
     }
 
@@ -24,98 +28,95 @@ async function getTask(req: Request, res: Response): Promise<void>{
 }
 
 async function listTasks(req: Request, res: Response): Promise<void> {
-    try {
-        const tasks = await prisma.task.findMany();
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({ error: 'Error, could not be listed' });
-    }
+  try {
+    const tasks = await prisma.task.findMany();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: 'Error, could not be listed' });
+  }
 }
 
 async function createTask(req: Request, res: Response): Promise<void> {
-    const { title, description, status, expireDate }: Task = req.body;
+  const { title, description, status, expireDate }: Task = req.body;
 
-    if(!title || !status) {
-        res.status(400).json({error: "Mandatory fields are missing!!!"}); 
-        return;
-    }
+  if (!title || !status) {
+    res.status(400).json({ error: 'Mandatory fields are missing!!!' });
+    return;
+  }
 
-    
-    if (!validStatus.includes(status)) {
-        res.status(400).json({ error: 'Invalid Status' });
-        return;
-    }
+  if (!validStatus.includes(status)) {
+    res.status(400).json({ error: 'Invalid Status' });
+    return;
+  }
 
-    try {
-        const task = await prisma.task.create({
-        data: {
-            title,
-            description,
-            status,
-            expireDate: expireDate ? new Date(expireDate) : null,
-        },
-        });
+  try {
+    const task = await prisma.task.create({
+      data: {
+        title,
+        description,
+        status,
+        expireDate: expireDate ? new Date(expireDate) : null,
+      },
+    });
 
-        res.status(201).json(task);
-    } catch (error) {
-        res.status(500).json({ error: 'Error, task could not be created' });
-    }
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ error: 'Error, task could not be created' });
+  }
 }
 
 async function updateTask(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-    const { title, description, status, expireDate }: Task = req.body;
-  
-    if (status) {
-      if (!validStatus.includes(status)) {
-        res.status(400).json({ error: 'task with invalid status' });
-        return;
-      }
-    }
-  
-    try {
-      const task = await prisma.task.update({
-        where: { id: Number(id) },
-        data: {
-          title,
-          description,
-          status,
-          expireDate: expireDate ? new Date(expireDate) : undefined,
-        },
-      });
-  
-      res.status(200).json(task);
-    } catch (error) {
-      res.status(500).json({ error: 'Error: task could not be updated' });
-    }
-  };
+  const { id } = req.params;
+  const { title, description, status, expireDate }: Task = req.body;
 
-  async function deleteTask(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-
-    try {
-        const task = await prisma.task.findUnique({
-            where: { id: Number(id) },
-          });
-      
-          if (!task) {
-            res.status(404).json({ error: 'task could not be found with this specific id: ' + id });
-            return;
-          }
-      await prisma.task.delete({
-        where: { id: Number(id) },
-      });
-  
-      res.status(204).send();
-    } catch (error) {
-      res.status(500).json({ error: 'Error: task could not be deleted' });
+  if (status) {
+    if (!validStatus.includes(status)) {
+      res.status(400).json({ error: 'task with invalid status' });
+      return;
     }
   }
 
-export {
-    createTask,
-    listTasks,
-    getTask,
-    updateTask,
-    deleteTask
+  try {
+    const task = await prisma.task.update({
+      where: { id: Number(id) },
+      data: {
+        title,
+        description,
+        status,
+        expireDate: expireDate ? new Date(expireDate) : undefined,
+      },
+    });
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ error: 'Error: task could not be updated' });
+  }
 }
+
+async function deleteTask(req: Request, res: Response): Promise<void> {
+  const { id } = req.params;
+
+  try {
+    const task = await prisma.task.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!task) {
+      res
+        .status(404)
+        .json({
+          error: 'task could not be found with this specific id: ' + id,
+        });
+      return;
+    }
+    await prisma.task.delete({
+      where: { id: Number(id) },
+    });
+
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: 'Error: task could not be deleted' });
+  }
+}
+
+export { createTask, listTasks, getTask, updateTask, deleteTask };
