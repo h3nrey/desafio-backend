35b747986b30223efe842e386ea66389e7e784ea
pedorooo <phln@cin.ufpe.br>
2025-03-03 17:10:54 -0300

    fix: changing default port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api', routes);
