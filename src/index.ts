39ae0e83025499b9403b027ca367fe2ea2652fd7
pedorooo <phln@cin.ufpe.br>
2025-03-03 20:52:53 -0300

    FEAT: adding cors configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api', routes);
+
+// const origin = process.env.NODE_ENV === 'production' ? 'https://yourapp.com' : 'http://localhost:3000';
+const origins = ['http://localhost:3000', 'http://54.210.97.220'];
+
+app.use(cors({
+  origin: origins
+}))
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
