e6f81595da11991b23de86c0ebc0668b706e927b
pedorooo <phln@cin.ufpe.br>
2025-03-04 00:39:45 -0300

    FIX: updating cors origin port from prod endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // const origin = process.env.NODE_ENV === 'production' ? 'https://yourapp.com' : 'http://localhost:3000';
-const origins = ['http://localhost:3000', 'http://54.210.97.220'];
+const origins = ['http://localhost:3000', 'http://54.210.97.220:3000'];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
