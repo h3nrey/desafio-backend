95f062f9f49b96e204a5e8fd974f7c8c5d046b39
pedorooo <phln@cin.ufpe.br>
2025-03-04 09:53:38 -0300

    FEAT: updating origins to cors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // const origin = process.env.NODE_ENV === 'production' ? 'https://yourapp.com' : 'http://localhost:3000';
-const origins = ['http://localhost:3000'];
+const origins = ['http://localhost:3000', 'http://172.31.92.207', 'http://54.210.97.220'];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
